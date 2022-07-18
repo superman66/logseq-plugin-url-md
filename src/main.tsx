498bc5a514d7bfcee0ce9f5f03d0d39a6f7c44fe
huichao.chen <huichao.chen@mail.hypers.com>
2022-07-18 13:53:09 +0800

    feat: 使用 clipboard 获取剪贴板内容

--- src/main.tsx
+++ src/main.tsx
@@ -4,34 +4,52 @@ import { getLinkMDTitle } from "./utils";
 
 const pluginId = PL.id;
 
-function main() {
+async function readClipboard() {
+  if (!navigator.clipboard) {
+    // Clipboard API not available
+    return;
+  }
+
+  try {
+    const text = await navigator.clipboard.readText();
+    console.log("当前粘贴内容：", text);
+    const convert = await getLinkMDTitle(text);
+    console.log("转换后格式", convert);
+  } catch (err) {
+    console.error("Failed to copy!", err);
+  }
+}
+
+async function main() {
   console.info(`#${pluginId}: MAIN`);
-  const mainContentContainer = parent.document.getElementById(
-    "main-content-container"
-  );
-  console.log(mainContentContainer);
+  // const mainContentContainer = parent.document.getElementById(
+  //   "main-content-container"
+  // );
+  // console.log(mainContentContainer);
 
-  const handlePaste = async (event: ClipboardEvent) => {
-    if (event.clipboardData && event?.clipboardData?.files?.length > 0) {
-      return;
-    }
+  // const handlePaste = async (event: ClipboardEvent) => {
+  //   if (event.clipboardData && event?.clipboardData?.files?.length > 0) {
+  //     return;
+  //   }
 
-    if (event.clipboardData) {
-      const text = event.clipboardData.getData("text/plain");
-      if (text !== "") {
-        event.preventDefault();
-        event.stopPropagation();
-        console.log("当前粘贴内容：", text);
-        const convert = await getLinkMDTitle(text);
-        console.log("转换后格式", convert);
-        await logseq.Editor.insertAtEditingCursor(convert);
-        return;
-      }
-    }
-  };
+  //   if (event.clipboardData) {
+  //     const text = event.clipboardData.getData("text/plain");
+  //     if (text !== "") {
+  //       event.preventDefault();
+  //       event.stopPropagation();
+  //       console.log("当前粘贴内容：", text);
+  //       const convert = await getLinkMDTitle(text);
+  //       console.log("转换后格式", convert);
+  //       await logseq.Editor.insertAtEditingCursor(convert);
+  //       return;
+  //     }
+  //   }
 
-  mainContentContainer &&
-    mainContentContainer.addEventListener("paste", handlePaste);
+  logseq.Editor.registerSlashCommand("md-link", async () => {
+    //insertAtEditingCursor 这个函数是向logseq当前光标处插入传参的内容
+    await readClipboard();
+    // await logseq.Editor.insertAtEditingCursor();
+  });
 }
 
 logseq.ready(main).catch(console.error);
